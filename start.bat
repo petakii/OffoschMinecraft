a52e7bc51311eebbf670bc3389df41609622b00b
petakii <petaki96@gmail.com>
2020-03-14 02:51:35 +0100

    first

---
+++ start.bat
@@ -0,0 +1,13 @@
+git init
+git clone https://github.com/petakii/OffoschMinecraft.git .
+git remote add origin https://github.com/petakii/OffoschMinecraft.git
+git reset --hard
+git pull https://github.com/buruska97/OffoschMinecraft.git
+
+java -Xms2G -Xmx2G -jar paper-133.jar
+
+git add *
+git rm --cached START.bat
+git commit -a -m "autoupdate %date%-%time%"
+git push origin master
+pause
