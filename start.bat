e44f0dcc41b4befd0d35e3455cf93077b1428b9b
petakii <petaki96@gmail.com>
2020-03-14 21:22:37 +0100

    re

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@ git init
 git clone https://github.com/petakii/OffoschMinecraft.git .
 git remote add origin https://github.com/petakii/OffoschMinecraft.git
 git reset --hard
-git pull https://github.com/buruska97/OffoschMinecraft.git
+git pull https://github.com/petakii/OffoschMinecraft.git
 
 java -Xms2G -Xmx2G -jar paper-133.jar
 
